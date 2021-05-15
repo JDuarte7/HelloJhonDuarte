a557fe2e5a028612b78f721df8cc8d01abf00e9b
John Duarte <johnduarte7@gmail.com>
2021-05-15 16:50:49 -0500

    Upload my project with my name Jhon Duarte

---
+++ src/duarte/Main.java
@@ -0,0 +1,11 @@
+package duarte;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String name = "Jhon Duarte";
+
+        System.out.println("Hello my name is " + name);
+    }
+}
